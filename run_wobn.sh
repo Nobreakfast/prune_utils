92181923585200d97603e3dc22c75bf6685f1989
Nobreakfast <zcl.neber@hotmail.com>
2024-02-02 10:43:54 +0800

    Remove unnecessary shell scripts

--- run_wobn.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-bash run_cifar10_prune.sh conv3_wobn kaiming_in $1
-bash run_cifar10_prune.sh conv3_wobn kaiming_out $1
-bash run_cifar10_prune.sh conv3_wobn xavier $1
-bash run_cifar10_prune.sh fc3_wobn kaiming_in $1
-bash run_cifar10_prune.sh fc3_wobn kaiming_out $1
-bash run_cifar10_prune.sh fc3_wobn xavier $1
