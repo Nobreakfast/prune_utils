Fix weight normalization and mean subtraction in model modules

cifar10.py
@@ -101,6 +101,14 @@ if __name__ == "__main__":
         from models.resnet_ori import resnet18
 
         model = resnet18(num_classes=10)
+    elif args.model == "resnet101":
+        from models.resnet_ori import resnet101
+
+        model = resnet101(num_classes=10)
+    elif args.model == "resnet101_wobn":
+        from models.resnet_ori_wobn import resnet101
+
+        model = resnet101(num_classes=10)
     elif args.model[:2] == "fc":
         import models.fc as fc
 
@@ -182,16 +190,20 @@ if __name__ == "__main__":
             model = model.to(device)
             example_data = torch.randn(1, 3, 32, 32)
             sign_dict = linearize(model)
-            iterations = 100
+            iterations = 10
             for i in range(iterations):
                 for module in model.modules():
                     if isinstance(module, nn.Conv2d):
+                        mean = module.weight[module.weight != 0].mean().item()
+                        module.weight_orig.data -= mean
                         sn = torch.linalg.norm(
                             module.weight.view(module.weight.shape[0], -1), ord=2
                         ).item()
                         # print(sn)
                         module.weight.data /= sn
                     elif isinstance(module, nn.Linear):
+                        mean = module.weight[module.weight != 0].mean().item()
+                        module.weight_orig.data -= mean
                         sn = torch.linalg.norm(module.weight, ord=2).item()
                         module.weight.data /= sn
                         # print(sn)

imagenet.py
@@ -166,51 +166,6 @@ def train(
             writer.add_scalar("top-5", top5_accuracy, epoch)
             writer.add_scalar("test loss", test_loss / len(testloader), epoch)
 
-    # print(
-    #     "[GPU {:d}] Best top-1: {:.2f}% top-5: {:.2f}%".format(
-    #         rank, best_top1, best_top5
-    #     )
-    # )
-    # if rank == 0:
-    #     model.load_state_dict(torch.load(save_path + "/best.pth"))
-    #     correct_top1 = 0
-    #     correct_top5 = 0
-    #     total_top1 = 0
-    #     total_top5 = 0
-    #     dataloader = DataLoaderX(
-    #         testset,
-    #         batch_size=128 * 4,
-    #         num_workers=16,
-    #         pin_memory=True,
-    #         shuffle=False,
-    #     )
-    #     model.eval()
-    #     with torch.no_grad():
-    #         test_loss = 0.0
-    #         for data in testloader:
-    #             images, labels = data
-    #             images, labels = images.to(device), labels.to(device)
-    #             outputs = model(images)
-    #             # top-1
-    #             _, predicted = torch.max(outputs.data, 1)
-    #             test_loss += criterion(outputs, labels).item()
-    #             total_top1 += labels.size(0)
-    #             correct_top1 += (predicted == labels).sum().item()
-
-    #             # top-5
-    #             _, predicted = torch.topk(outputs.data, 5, 1)
-    #             total_top5 += labels.size(0)
-    #             for i in range(5):
-    #                 correct_top5 += (predicted[:, i] == labels).sum().item()
-
-    #     top1_accuracy = 100 * correct_top1 / total_top1
-    #     top5_accuracy = 100 * correct_top5 / total_top5
-    #     print(
-    #         "Overall top-1: {:.2f}% top-5: {:.2f}%".format(top1_accuracy, top5_accuracy)
-    #     )
-    #     writer.add_scalar("Overall top-1", top1_accuracy, 0)
-    #     writer.add_scalar("Overall top-5", top5_accuracy, 0)
-
     # Close TensorBoard writer
     if writer is not None:
         writer.close()
@@ -263,14 +218,23 @@ if __name__ == "__main__":
         from models.resnet_imagenet import resnet50
 
         model = resnet50(num_classes=1000)
-    elif args.model == "resnet50_res":
-        from models.resnet_imagenet_res import resnet50
-
-        model = resnet50(num_classes=1000, alpha=args.alpha, beta=args.beta)
     else:
         raise ValueError("model not found")
 
-    initialization(model, args.im)
+    if args.im == "lsuv":
+        from lsuv import lsuv_with_dataloader
+
+        device = torch.device(f"cuda:0" if torch.cuda.is_available() else "cpu")
+        model = model.to(device)
+        model = lsuv_with_dataloader(model, trainloader, device=device)
+    elif args.im == "lipz":
+        initialization(model, "kaiming_in")
+        repair_model(model, 1)
+    elif args.im == "rei":
+        initialization(model, "kaiming_in")
+        repair_model(model, 3)
+    else:
+        initialization(model, args.im)
 
     if args.prune != 0.0:
         print(f"Original Sparsity: {cal_sparsity(model)}%")
@@ -303,6 +267,36 @@ if __name__ == "__main__":
             sign_dict = linearize(model)
             iterations = 100
             for i in range(iterations):
+                prune_ratio = args.prune / iterations * (i + 1)
+                score_dict = synflow(model, example_data)
+                threshold = cal_threshold(score_dict, prune_ratio)
+                if i != iterations - 1:
+                    apply_prune(model, score_dict, threshold)
+                    remove_mask(model)
+                else:
+                    nonlinearize(model, sign_dict)
+                    apply_prune(model, score_dict, threshold)
+            model = model.to(torch.device("cpu"))
+        elif args.algorithm == "synflow_repair":
+            device = torch.device(f"cuda:0" if torch.cuda.is_available() else "cpu")
+            model = model.to(device)
+            example_data = torch.randn(1, 3, 224, 224)
+            sign_dict = linearize(model)
+            iterations = 10
+            for i in range(iterations):
+                for name, m in model.named_modules():
+                    if isinstance(m, nn.Conv2d):
+                        mean = module.weight[module.weight != 0].mean().item()
+                        module.weight_orig.data -= mean
+                        sn = torch.linalg.norm(
+                            m.weight.view(m.weight.shape[0], -1), ord=2
+                        ).item()
+                        m.weight_orig.data /= sn
+                    elif isinstance(m, nn.Linear):
+                        mean = module.weight[module.weight != 0].mean().item()
+                        module.weight_orig.data -= mean
+                        sn = torch.linalg.norm(m.weight, ord=2).item()
+                        m.weight_orig.data /= sn
                 prune_ratio = args.prune / iterations * (i + 1)
                 score_dict = synflow(model, example_data)
                 threshold = cal_threshold(score_dict, prune_ratio)